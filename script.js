c53e5fe8d4121d76200bbc71db3dc0c2a94172c5
Parthik <parthikdas@gmail.com>
2021-11-07 23:13:37 +0530

    Finally Done

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let boxLeft = box.offsetLeft
 let dino = document.getElementById('dino')
 let score = document.getElementById('score')
 let realScore = 0
-let highscore = document.getElementById('highScore')
+let highScore = document.getElementById('highScore')
 let highestScore = 0
 let spaceClicked = true // to prevent from again jumping while it is jumping
 let gameAlreadyOver = false // to prevent the other obstacle from moving
@@ -44,43 +44,43 @@ document.body.onkeyup = function(e) {
 }
 
 //Video Streaming and All
-// let video = document.getElementById('video')
-// let model
-// let canvas = document.getElementById('canvas')
-// let ctx = canvas.getContext('2d')
+let video = document.getElementById('video')
+let model
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getContext('2d')
 
-// const videoStream = () => {
-//     navigator.mediaDevices.getUserMedia({ // getUserMedia is a API and takes an object as an argument 
-//         video: {width: 350, height: 250},
-//         audio: false // make it true if u also want audio
-//     })
-//     .then((stream) => { // and this returns a stream object which is a media stream object
-//         video.srcObject = stream 
-//     })
-// }
-// const detectFaces = async () => { // this is function is asynchronus
-//     const prediction = await model.estimateFaces(video, false) // 1st arg is input, 2nd is if we want to return tensor or not So her we dont want tensor we want actual values of coordinates
-//     ctx.drawImage(video, 0, 0, 350, 250)
-//     // a line to tell the limit
-//     ctx.beginPath();
-//     ctx.lineWidth = '2'
-//     ctx.strokeStyle = 'red'
-//     ctx.moveTo(0, 100);
-//     ctx.lineTo(350, 100);
-//     ctx.stroke();
+const videoStream = () => {
+    navigator.mediaDevices.getUserMedia({ // getUserMedia is a API and takes an object as an argument 
+        video: {width: 350, height: 250},
+        audio: false // make it true if u also want audio
+    })
+    .then((stream) => { // and this returns a stream object which is a media stream object
+        video.srcObject = stream 
+    })
+}
+const detectFaces = async () => { // this is function is asynchronus
+    const prediction = await model.estimateFaces(video, false) // 1st arg is input, 2nd is if we want to return tensor or not So her we dont want tensor we want actual values of coordinates
+    ctx.drawImage(video, 0, 0, 350, 250)
+    // a line to tell the limit
+    ctx.beginPath();
+    ctx.lineWidth = '2'
+    ctx.strokeStyle = 'red'
+    ctx.moveTo(0, 100);
+    ctx.lineTo(350, 100);
+    ctx.stroke();
 
-//     prediction.forEach(pred => { // pred corresponds to 1 face
-//         // plot nose point, its in landmark array
-//         ctx.fillStyle = 'yellow'
-//         ctx.fillRect(pred.landmarks[2][0], pred.landmarks[2][1], 5, 5)
-//         if(pred.landmarks[2][1] < 100) jump() // if limit touched jump
-//     })
-// }
-// videoStream()
-// video.addEventListener('loadeddata', async () =>{ // After the data is loaded then canvas is called, else it will not show
-//     model = await blazeface.load()
-//     setInterval(detectFaces, 40) // 1000/24frames = 40
-// })
+    prediction.forEach(pred => { // pred corresponds to 1 face
+        // plot nose point, its in landmark array
+        ctx.fillStyle = 'yellow'
+        ctx.fillRect(pred.landmarks[2][0], pred.landmarks[2][1], 5, 5)
+        if(pred.landmarks[2][1] < 100) jump() // if limit touched jump
+    })
+}
+videoStream()
+video.addEventListener('loadeddata', async () =>{ // After the data is loaded then canvas is called, else it will not show
+    model = await blazeface.load()
+    setInterval(detectFaces, 40) // 1000/24frames = 40
+})
 
 /* Game Functions */
 //Function to start the game
@@ -89,6 +89,11 @@ function start(){
 	spaceClicked = true
 	realScore = 0
 	score.innerHTML = realScore
+	document.getElementById("dino").getElementsByTagName("img")[0].src="jumper.jpeg";
+	let obs = document.getElementsByClassName('obs') // remove all obs if any
+	while(obs[0]) {
+		obs[0].parentNode.removeChild(obs[0])
+	}
 	doit = setInterval(move, 3000)
 }
 
@@ -109,7 +114,11 @@ function move(){// Function for creating and moving
 			document.getElementById("dino").getElementsByTagName("img")[0].src="fail.png";
 			clearInterval(movingDiv)
 			clearInterval(doit)
-			setTimeout(reset, 3000)
+			// Update high score
+			if(realScore > highestScore) {
+				highestScore = realScore
+				highScore.innerHTML = 'Highest Score : ' + highestScore
+			}
 		}
 		if(!gameAlreadyOver) {
 			if(margin > boxWidth) {
@@ -123,22 +132,8 @@ function move(){// Function for creating and moving
 			score.innerHTML = realScore
 		}
 	}
-
 	function gameOver(){ // Game over condition
 		// returns 1 if game is really over, 0  means nothing go on with interval	
 		return dino.offsetTop > div.offsetTop-div.offsetHeight && div.offsetLeft >= dino.offsetLeft && div.offsetLeft <= dino.offsetLeft+dino.offsetWidth ? 1 : 0;
-	}
-
-	function reset(){ // To remove the obs after 3 sec
-		document.getElementById("dino").getElementsByTagName("img")[0].src="jumper.jpeg";
-		let obs = document.getElementsByClassName('obs') // remove all obs
-		while(obs[0]) {
-			obs[0].parentNode.removeChild(obs[0])
-		}
-		// Update high score
-		if(realScore > highestScore) {
-			highestScore = realScore
-			highScore.innerHTML = 'Highest Score : ' + highestScore
-		}
 	}
 }
